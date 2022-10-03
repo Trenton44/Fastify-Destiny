95189941814d7547af83d2c850041cbc0810e07d
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-10-03 17:13:38 -0500

    Update manifest loading and Dockerfile -Now loads manifest files synchronously. Much slower, but async loading make 0B files. -Dockerfile should now work for production. temporary solution for ssl cert keys

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM node:latest
 WORKDIR /app
 COPY . .
+ADD local_key.pem /etc/pki/tls/
+ADD local_cert.pem /etc/pki/tls/
 RUN npm install
 RUN curl https://raw.githubusercontent.com/Bungie-net/api/master/openapi.json > bungie_api/openapi.json 
 RUN curl https://www.bungie.net/Platform/Destiny2/Manifest/ > bungie_api/manifest.json
 RUN node bungie_api/manifest.js
 EXPOSE 3000
-#CMD ["node", "server.js"]
+ENTRYPOINT npm run deploy
--- bungie_api/manifest.js
+++ bungie_api/manifest.js
@@ -1,16 +1,31 @@
 const fs = require("fs");
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 var manifest = require("./manifest.json");
+const { Console } = require("console");
 var bungie = "https://www.bungie.net";
 
+let promises = [];
 for (i in manifest.Response.jsonWorldContentPaths) {
   let path = manifest.Response.jsonWorldContentPaths[i];
   path = bungie + path;
   let savepath = __dirname + "/manifest/" + i;
   console.log("Pulling " + i + ": " + path);
   console.log("Saving to: " + savepath);
-  exec("mkdir -p " + savepath);
-  exec("curl " + path + " > " + savepath + "/world_content.json");
+  execSync("mkdir -p " + savepath);
+  execSync("curl " + path + " > " + savepath + "/world_content.json", (error, stdout, stderr) => {
+    if(error){
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  //promises.push(request);
+  //console.log("next up: "+manifest.Response.jsonWorldContentPaths[i+1]);
 }
 
-process.exit(0);
+Promise.all(promises)
+.then( (result) => {
+  console.log("Completed.");
+  process.exit(0);
+} )
+.catch( (error) => console.error(error) );
