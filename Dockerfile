81b56d23856a06b663c7db4640c471c008bb1175
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-10-11 13:12:52 -0500

    Updated manifest download -Manifest files are now downloaded asynchronously, on container deployment

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,5 @@ ADD local_cert.pem /etc/pki/tls/
 RUN npm install
 RUN curl https://raw.githubusercontent.com/Bungie-net/api/master/openapi.json > bungie_api/openapi.json 
 RUN curl https://www.bungie.net/Platform/Destiny2/Manifest/ > bungie_api/manifest.json
-RUN node bungie_api/manifest.js
 EXPOSE 3000
-#ENTRYPOINT npm run deploy
+ENTRYPOINT npm run deploy
--- bungie_api/manifest.js
+++ bungie_api/manifest.js
@@ -1,22 +1,49 @@
 const fs = require("fs");
+const https = require('https')
 const { exec, execSync } = require("child_process");
 var manifest = require("./manifest.json");
 const { Console } = require("console");
 var bungie = "https://www.bungie.net";
+var bungiepath = "https://www.bungie.net";
 
-for (i in manifest.Response.jsonWorldContentPaths) {
-  let path = manifest.Response.jsonWorldContentPaths[i];
-  path = bungie + path;
-  let savepath = __dirname + "/manifest/" + i;
-  console.log("Pulling " + i + ": " + path);
-  console.log("Saving to: " + savepath);
-  execSync("mkdir -p " + savepath);
-  execSync("curl " + path + " > " + savepath + "/world_content.json", (error, stdout, stderr) => {
-    if(error){
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
+function asyncDownloadManifest(manifest_url, manifest_lang, filename){
+  console.log("Starting Download for Manifest: "+manifest_lang);
+
+  let filepath = __dirname + "/manifest/" + manifest_lang;
+  execSync("mkdir -p " + filepath);
+  filepath = filepath+"/"+filename;
+
+  https.get(manifest_url, (response) => {
+    let file = fs.createWriteStream(filepath);
+    response.pipe(file);
+    file.on('finish', () => {
+      file.close();
+      console.log("Finished Downloading "+manifest_lang+": "+filepath);
+    })
   });
 }
 
+//Keeping as a backup, but async is much faster.
+function DownloadManifest(){
+  for (i in manifest.Response.jsonWorldContentPaths) {
+    let path = manifest.Response.jsonWorldContentPaths[i];
+    path = bungie + path;
+    let savepath = __dirname + "/manifest/" + i;
+    console.log("Pulling " + i + ": " + path);
+    console.log("Saving to: " + savepath);
+    execSync("mkdir -p " + savepath);
+    execSync("curl " + path + " > " + savepath + "/world_content.json", (error, stdout, stderr) => {
+      if(error){
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  }
+}
+
+console.log(manifest.messageStatus);
+for (i in manifest.Response.jsonWorldContentPaths){
+  let urlpath = bungiepath + manifest.Response.jsonWorldContentPaths[i];
+  asyncDownloadManifest(urlpath, i, "world_content.json");
+}
