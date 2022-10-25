3cd1b69eaee0b9ae5d8d82a0cfe8d40665dc58d7
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-10-24 20:55:43 -0500

    Added promise.then back to GetProfile endpoint Removed the global-config.js file.

--- endpoints/GetProfile/endpoint.js
+++ endpoints/GetProfile/endpoint.js
@@ -3,18 +3,21 @@ const path = require("path");
 const configuration = require("./config");
 
 const schema = require("./schema.json");
-const D2GetProfile = require(path.join(__dirname, "..", "..", "/bungie-api/api.js")).GetProfile;
 
 //The function used to process the receiving data.
 //This will ready the necessary data for the Bungie API call, and then pass this to a function inside wrapper.js
-async function getprofile(request, reply,){
+async function getprofile(request, reply){
     let membershipId = request.session.user_data.active_account_id;
     let membershipType = request.session.user_data.d2_account.destinyMemberships[membershipId].membershipType;
-    let components = request.query.components.toString();
-    console.log(components);
-    //D2GetProfile(membershipId, membershipType, )
-    //.then()
-    return reply.code(200).send({ status: "ok!" });
+    let components = request.query.components;
+    return request.d2api.GetProfile(membershipId, membershipType, components, request.session.auth_data.access_token, configuration)
+    .then( (data) => {
+        return data;
+    })
+    .catch( (error) => {
+        console.error(error);
+        return error;
+    });
 }
 
 let GetProfile = (fastify, options, next) => {
