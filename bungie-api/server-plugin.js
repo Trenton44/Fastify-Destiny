f5e10438b0ea05c8379969294b9186f499bb4267
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-10-26 22:19:54 -0500

    Added server-plugin.js -Makes use of fastify's decorators -Datamap and the api calls are now available to all endpoints through the Request Object.

---
+++ bungie-api/server-plugin.js
@@ -0,0 +1,18 @@
+const fp = require('fastify-plugin');
+const DataMap = require("./map.js");
+const D2API = require("./api.js");
+
+async function myPlugin (app) {
+  app.decorateRequest("MapResponse", null);
+  app.decorateRequest("APIRequest", null);
+  app.addHook('onRequest', async (req, reply) => {
+    let language = req.session.userlanguage;
+    console.log(language);
+    if(!language){ language = "en"; }
+    let manifest = require("./manifest/"+language+"/manifest.json");
+    req.MapResponse = new DataMap(manifest);
+    req.APIRequest = D2API;
+  });
+}
+
+module.exports = fp(myPlugin)
