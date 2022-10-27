5f3f2d78974fed5026d6945fe2dc1a704c7e6daa
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-10-26 22:20:32 -0500

    Updated variables of manifest.js -Should better represent what they are for

--- bungie-api/manifest.js
+++ bungie-api/manifest.js
@@ -2,22 +2,21 @@ const fs = require("fs");
 const https = require('https')
 const { execSync } = require("child_process");
 var manifest = require("./manifest.json");
-var bungie = "https://www.bungie.net";
 var bungiepath = "https://www.bungie.net";
 
-function asyncDownloadManifest(manifest_url, manifest_lang, filename){
-  console.log("Starting Download for Manifest: "+manifest_lang);
+function asyncDownloadManifest(manifesturl, language, filename){
+  console.log("Starting Download for Manifest: "+language);
 
-  let filepath = __dirname + "/manifest/" + manifest_lang;
+  let filepath = __dirname + "/manifest/" + language;
   execSync("mkdir -p " + filepath);
   filepath = filepath+"/"+filename;
 
-  https.get(manifest_url, (response) => {
+  https.get(manifesturl, (response) => {
     let file = fs.createWriteStream(filepath);
     response.pipe(file);
     file.on('finish', () => {
       file.close();
-      console.log("Finished Downloading "+manifest_lang+": "+filepath);
+      console.log("Finished Downloading "+language+": "+filepath);
     })
   });
 }
@@ -31,7 +30,7 @@ function DownloadManifest(){
     console.log("Pulling " + i + ": " + path);
     console.log("Saving to: " + savepath);
     execSync("mkdir -p " + savepath);
-    execSync("curl " + path + " > " + savepath + "/world_content.json", (error, stdout, stderr) => {
+    execSync("curl " + path + " > " + savepath + "/manifest.json", (error, stdout, stderr) => {
       if(error){
         console.error(`exec error: ${error}`);
         return;
@@ -41,8 +40,8 @@ function DownloadManifest(){
   }
 }
 
-console.log(manifest.messageStatus);
+
 for (i in manifest.Response.jsonWorldContentPaths){
   let urlpath = bungiepath + manifest.Response.jsonWorldContentPaths[i];
-  asyncDownloadManifest(urlpath, i, "world_content.json");
+  asyncDownloadManifest(urlpath, i, "manifest.json");
 }
