31698a8b6f599f225bbccd582fcc9520812bc891
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-02 21:32:15 -0500

    -Added more logic for "link" keyword -Now selects target node if ! is in first char of key Connects linked nodes pre-transform

--- bungie-api/nodecontroller.js
+++ bungie-api/nodecontroller.js
@@ -50,30 +50,57 @@ class NodeController {
     compileTree(){
         if(this.config.splice)
             this.root.pruneChildren(this.config.splice);
+        this.#compileLinks(this.root, true);
+        if(Object.keys(this.links) != 0)
+            this.#ConnectLinks(); // connect all of the linked data together
         return this.root.compile();
     }
-    compileLinks(node, reset=false){
+    #pushLink(key, node, istarget){
+        if(!this.links[key])
+            this.links[key] = [];
+        if(istarget)
+            this.links[key].unshift(node); // add target node to front of array
+        else
+            this.links[key].push(node); // add node to the list
+        return true;
+    }
+    #compileLinks(node, reset=false){
         if(reset){ this.links = {}; }
-        for(let child of node.children){
-            if(child.options.link){
-                if(child.options.link == "key"){
-                    if(!this.links[child.key]){ this.links[child.key] = []; }
-                    this.links[child.key].push(child);
-                }
-                else {
-                    for(let childnode of child){
-                        if(childnode.key == child.options.link){
-                            if(!this.links[childnode.data]){ this.links[childnode.data]; }
-                            this.links[childnode.data].push(child);
-                        }
-                    }
-                    if(!this.links[child.options.link]){ this.links[child.options.link] = []; }
-                    this.links[child.options.link].push(child);
-                }
+        node.children.forEach( (child) => {
+            let link = child.options.link;
+            if(!link)
+                return false; //if link is undefined or false, no need to check any further
+            let istarget = false; // whether this node is the target for all others to be appended to
+            if(link[0] == "!"){ 
+                istarget = true; 
+                link = link.slice(1); //remove exclamation point, it'll mess up comparisons
             }
-        }
-        console.log("done");
-        console.log(this.links);
+
+            // if the value of the option is "key", we want to use this node's key to link the data
+            // useful for linking two objects, like a character's inventory and equipment, which are under two different object, but both keyed by the character's id.
+            if(link == "key"){
+                this.#pushLink(child.key, child, target);
+                return true;
+            }
+            // if it is another value, we want to link this node with others based on the data value of one of it's children.
+            // Ex: itemInstanceComponents are keyed by itemInstanceId, character items have a child node called "itemInstanceId".
+            // we link that node by it's itemInstanceId key to 
+            let linkvalue = child.children.find( (cchild) => cchild.key == link);
+            if(linkvalue == -1)
+                return false; // child key didn't match desired key, move to next child.
+            linkvalue = linkvalue.data; // use the data value of the child node
+            child.parentkey = child.parent.key; // since all of them are being linked together by one key, we'll use the parent's key to differentiate which came from where
+            this.#pushLink(linkvalue, child, istarget);
+        });
+
+        node.children.forEach((child) => this.compileLinks(child)); //check each subsequent child for any "link" option keywords
+        return true;
+    }
+    #ConnectLinks(){
+        Object.entries(this.links).forEach( ([linkkey, nodelist]) => {
+            let target = nodelist.pop(); // first element in each list is target, which was decided by the ! in the link option's value
+            nodelist.forEach( (node) => target.addChild(node));
+        });
     }
 }
 
