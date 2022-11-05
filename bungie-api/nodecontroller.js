72e044f69bed605c6d35c11b1e0b6f6529fd8522
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-05 13:44:37 -0500

    Refactored NodeController -Condensed add/delete node functions to one each -Added function to move child node between parents -Refactored logic for "link" keyword to one function, connectLinks()

--- bungie-api/nodecontroller.js
+++ bungie-api/nodecontroller.js
@@ -2,105 +2,89 @@ const guide = require("./json-schema-controller.js");
 const Node = require("./node.js");
 
 class NodeController {
-    constructor(config){
-        //  hierarchy is effectively a copy of the openapi schema, at initial creation.
-        //  it then may deviate based on user config, and this is to act as a new "schema" that maintains those changes
-        this.hierarchy = {};
-        this.root = null;
+    constructor(config={}){
         this.config = config;
-        this.links = {};
+        this.root = null;
     }
-    #searchNode(keylist){
+    searchNode(keylist){
         let node = this.root;
         while(keylist.length > 0){
-            node = node.find((child) => child.key == keylist.shift());
-            if(!(node instanceof Node)){ return false; }
+            node = node.find( (child) => child.key == keylist.shift()) || false;
+            if(!node){ return false; }
         }
         return node;
     }
-    #removeNodeFromLocation(keylist){
-        let node = this.#searchNode(keylist);
-        if(!node)
-            throw Error("Unable to find Node with the given keylist");
-        this.#removeChildNode(node);
-        return true;
-    }
-    #removeChildNode(node){ node.removeSelf(); }
-    removeNode(nodelocation){
-        if(nodelocation instanceof Node)
-            this.#removeChildNode(nodelocation);
+    addNode(keylist, node){
+        let parent = this.searchNode(keylist);
+        if(parent instanceof Node)
+            parent.addChild(node);
         else
-            this.#removeNodeFromLocation(nodelocation);
+            throw Error("Unable to find node with the given keylist.");
         return true;
     }
-    #addChildNode(parent, child){ parent.addChild(child); }
-    #addChildToLocation(keylist, child){
-        let parent = this.#searchNode(keylist);
-        if(!node)
-            throw Error("Unable to find parent node with the given keylist");
-        this.#addChildNode(parent, child);
-    }
-    addNode(nodelocation, child){
-        if(nodelocation instanceof Node)
-            this.#addChildNode(nodelocation, child)
-        else
-            this.#addChildToLocation(nodelocation, child);
+    deleteNode(nodelocation){
+        if(nodelocation instanceof Node){
+            nodelocation.delete();
+            return true;
+        }
+        let node = this.searchNode(nodelocation);
+        if(!(node instanceof Node))
+            throw Error("Unable to find node with the given keylist.");
+        node.delete();
         return true;
+        
     }
+    moveNode(newparent, node){
+        node.parent.removeChild(node);
+        newparent.addChild(node);
+    }
+    #connectLinks(){
+        let links = {};
+        let linkNodes = this.#getNodesByOption("link");
+        linkNodes.forEach( (node) => {
+            let link = node.options.link;
+            let targetnode = false;
+            let keyused = child.key;
+            let newkey = child.parent.key;
+            if(link[0] = "!"){
+                targetnode = true;
+                link = link.slice(1);
+            }
+            if(link != "key"){
+                let linkvar = child.children.find( (cchild) => cchild.key == link);
+                if(linkvar == -1){ throw Error("Could not find child "+link+" in node "+child.key); }
+                keyused = linkvar.data;
+                newkey = keyused+"Mapped";
+            }
+            if(!links[keyused]){ links[keyused] = []; }
+            if(istarget)
+                links[keyused].unshift(child);
+            else
+                links[keyused].push(child);
+            child.key = newkey;
+        });
+        Object.entries(links).forEach( ([linkkey, linklist]) =>{
+            let targetnode = linklist.shift();
+            while(linklist.length > 0)
+                this.moveNode(targetnode, linklist.shift());
+            delete links[linkkey]; //remove entirely
+        });
+        return true; // links should once again be an empty object by this point
+    }
+    #getNodesByOption(key){ return this.root.getChildrenWithOption(key); }
+    #getNodesByKey(key){ return this.root.getChildrenMatchingKey(key); }
     compileTree(){
-        if(this.config.splice)
-            this.root.pruneChildren(this.config.splice);
-        this.#compileLinks(this.root, true);
-        if(Object.keys(this.links) != 0)
-            this.#ConnectLinks(); // connect all of the linked data together
-        return this.root.compile();
-    }
-    #pushLink(key, node, istarget){
-        if(!this.links[key])
-            this.links[key] = [];
-        if(istarget)
-            this.links[key].unshift(node); // add target node to front of array
-        else
-            this.links[key].push(node); // add node to the list
-        return true;
-    }
-    #compileLinks(node, reset=false){
-        if(reset){ this.links = {}; }
-        node.children.forEach( (child) => {
-            let link = child.options.link;
-            if(!link)
-                return false; //if link is undefined or false, no need to check any further
-            let istarget = false; // whether this node is the target for all others to be appended to
-            if(link[0] == "!"){ 
-                istarget = true; 
-                link = link.slice(1); //remove exclamation point, it'll mess up comparisons
-            }
-
-            // if the value of the option is "key", we want to use this node's key to link the data
-            // useful for linking two objects, like a character's inventory and equipment, which are under two different object, but both keyed by the character's id.
-            if(link == "key"){
-                this.#pushLink(child.key, child, target);
-                return true;
-            }
-            // if it is another value, we want to link this node with others based on the data value of one of it's children.
-            // Ex: itemInstanceComponents are keyed by itemInstanceId, character items have a child node called "itemInstanceId".
-            // we link that node by it's itemInstanceId key to 
-            let linkvalue = child.children.find( (cchild) => cchild.key == link);
-            if(linkvalue == -1)
-                return false; // child key didn't match desired key, move to next child.
-            linkvalue = linkvalue.data; // use the data value of the child node
-            child.parentkey = child.parent.key; // since all of them are being linked together by one key, we'll use the parent's key to differentiate which came from where
-            this.#pushLink(linkvalue, child, istarget);
-        });
-
-        node.children.forEach((child) => this.compileLinks(child)); //check each subsequent child for any "link" option keywords
-        return true;
-    }
-    #ConnectLinks(){
-        Object.entries(this.links).forEach( ([linkkey, nodelist]) => {
-            let target = nodelist.pop(); // first element in each list is target, which was decided by the ! in the link option's value
-            nodelist.forEach( (node) => target.addChild(node));
-        });
+        this.#connectLinks();
+        if(this.config.splice){
+            let splicenodes = [...this.#getNodesByKey("data"), ...this.#getNodesByKey("items")];
+            splicenodes.forEach( (node) => {
+                let parent = node.parent;
+                let children = node.children;
+                node.delete();
+                children.forEach( (child) => parent.addChild(child));
+            });
+        }
+        return this.root.compile(); 
     }
 }
 
