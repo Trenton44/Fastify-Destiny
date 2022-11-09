ced2d35e80cd40076944d73cbb445f1c35436439
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-08 22:47:17 -0600

    Reworking endpoints to be a little more condensed -Reworking how endpoints build requests currently invalidates need for seperate files -So recombining all endpoint URL's to endpoints.js

--- bungie-api/server-plugin.js
+++ bungie-api/server-plugin.js
@@ -2,7 +2,7 @@ const fp = require('fastify-plugin');
 const DataMap = require("./map.js");
 const D2API = require("./api.js");
 
-async function myPlugin (app) {
+async function myPlugin(app) {
   app.decorateRequest("MapResponse", null);
   app.decorateRequest("APIRequest", null);
   app.addHook('onRequest', async (req, reply) => {
--- endpoints/endpoints.js
+++ endpoints/endpoints.js
@@ -1,42 +1,40 @@
-
 /**
  * @module Endpoints 
 */
-const authOsession = require("./AuthO/validate.js");
-const Login = require("./AuthO/endpoint.js");
-const Root = require("./Root/endpoint.js");
-const GetProfile = require("./GetProfile/endpoint.js");
-const GetCharacter = require("./GetCharacter/endpoint.js");
-const GetItem = require("./GetItem/endpoint.js");
+const Root = require("./root.js");
+const Login = require("./login.js");
+const validate = require("./session.js");
 
-//all /api endpoints that do not require authorization with bungie.
+const GetProfileSchema = require("../schemas/GetProfile.json");
 
-let api_noauth = (fastify, options, next) => {
-    //nothing necessary in the prehandler atm, but I'm keeping it for consistency with authorized endpoints
-    fastify.addHook('preHandler', async function(request, reply){ return true; });
-    //these two /api endpoints are for authorizing with the bungie api, and processing the response from bungie
-    fastify.register(Login)
-    fastify.register(Root); //use the root as a health check, and to set cookies on first access
-
+let general = (fastify, options, next) => {
+    fastify.register(Root);
+    fastify.register(Login);
     next();
 };
 
-// Contains all endpoints that require bungie api authorization to access
-// prehandler at start verifies that autorization is granted, returns error otherwise.
-// These endpoints should only be used by fronted to obtain data, not load webpages.
-let api_auth = (fastify, options, next) => {
-    //returns error if validation fails.
-    fastify.addHook('preHandler', async(request, reply) => {
-        return authOsession(request.session)
-        .catch( (error) => { return reply.code(400).send({ error: error })});
+let user = (fastify, options, next) => {
+    fastify.addHook('preHandler', async (request, reply) => {
+        return validate(request.session)
+        .catch( (error) => reply.code(400).send({error: error}));
     });
-    //endpoints accessible to appliation
-    fastify.register(GetProfile);
-    //fastify.register(GetCharacter);
-    //fastify.register(GetItem);
     
+    fastify.get("/GetProfile", { schema, GetProfileSchema }, async (request, reply) =>{
+        const openapiurl = "/Destiny2/{membershipType}/Profile/{destinyMembershipId}/";
+        let pathparams = {
+            destinyMembershipId: request.session.user_data.active_account_id,
+            membershipType: request.session.user_data.d2_account.destinyMemberships[activeId].membershipType,
+        };
+        let queryparams = { components: request.query.components };
+        let components = request.query.components;
+        let token = request.session.auth_data.access_token;
+        return request.APIRequest(openapiurl, pathparams, queryparams, token);
+    });
+    next();
+};
+
+module.exports = (fastify, options, next) => {
+    fastify.register(general);
+    fastify.register(user);
     next();
 }
-
-
-module.exports = {api_noauth, api_auth};
