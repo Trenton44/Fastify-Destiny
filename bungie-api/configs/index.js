3dfbd571659f072fee2d7afc0fcfa8f5620568e3
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-11-12 11:20:21 -0600

    Added index.js to /manifests & /configs -Handles the import/export of all related files

---
+++ bungie-api/configs/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    GetProfile: require("./GetProfile.json")
+};
---
+++ bungie-api/manifests/index.js
@@ -0,0 +1,5 @@
+/* this will need to look into 
+/manifest and check each folder.
+add the folder as the key, and the json file inside as the data
+then export that object, so program can read all manfiests
+*/
