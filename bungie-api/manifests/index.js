65903bcc84d1672f1c1ce0d0d16157381e02c048
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-11-12 15:41:20 -0600

    index.js now loads all manifest files into object, makes them available to server

--- bungie-api/manifests/index.js
+++ bungie-api/manifests/index.js
@@ -4,8 +4,11 @@ add the folder as the key, and the json file inside as the data
 then export that object, so program can read all manfiests
 */
 
-const manifests = "getall manifests into object.";
-
+const manifests = {};
+const fs = require("fs");
+fs.readdirSync(__dirname+"/manifest").forEach( (file) => {
+    manifests[file] = require("./manifest/"+file+".json");
+});
 module.exports = function(language){
     return manifests[language] ? manifests[language] : manifests["en"];
 }
