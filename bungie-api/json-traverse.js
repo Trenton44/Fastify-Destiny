39347423e64dc166d4d7a9e60e5e5571c4601a07
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-11-12 11:19:39 -0600

    renamed json-schema-controller to json-traverse -It's mostly for jsonschemas, but has some utility for objects in general -small refactoring using ternary operators

---
+++ bungie-api/json-traverse.js
@@ -0,0 +1,36 @@
+const api_doc = require("./openapi.json");
+
+function tryTraversal(keylist, obj){
+    try{
+        keylist.forEach( (key) => obj = obj[key]);
+    }
+    catch{
+        return false;
+    }
+    let obj = obj instanceof Object ? obj : false;
+}
+
+function parseRef(reflink, delim){
+    delim = !delim ? "/" : delim;
+    let refkeys = reflink.split(delim);
+    return refkeys[0] === "#" ? refkeys.slice(1) : refkeys;
+}
+
+function findSchema(refkeys, schema){
+    if(!schema){ schema = api_doc; }
+    let obj = tryTraversal(refkeys, schema);
+    if(!obj)
+        return [ false, false ];
+    return !obj["$ref"] ? [ obj, false ] : [ obj, obj["$ref"] ];
+}
+
+function findPath(oalink, reqtype, code=200, repformat="application/json"){
+    if(!oalink || !reqtype)
+        throw Error("findPath() requires an open api path key & the request type.");
+    let keys = [ "paths", oalink, reqtype, "responses", code ];
+    let schema = findSchema(keys)[0];
+    if(!schema){ throw Error("Unable to find schema for the given request path."); }
+    keys = [ "content", repformat, "schema", "properties", "Response"];
+    schema = findSchema(keys, schema);
+    return schema;
+}
