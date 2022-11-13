fb1231acdad15f6d00211e9f1faa8758e2e4d3a8
Trenton Choate <38508613+Trenton44@users.noreply.github.com>
2022-11-13 10:18:18 -0600

    Re-added input schema for GetProfile

--- bungie-api/schemas/index.js
+++ bungie-api/schemas/index.js
@@ -1,6 +1,8 @@
-const schemas = {};
 const fs = require("fs");
+const schemas = {};
+
 fs.readdirSync(__dirname+"/schemas").forEach( (file) => {
     schemas[file] = require("./schemas/"+file+".json");
 });
+
 module.exports = schemas;
