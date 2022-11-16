a18781ef48cbc5f5bb879422c526454dca26f885
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-15 18:19:21 -0600

    Removed axios request logic from plugin -> api.js -added import for buildAPIRequest for session

--- bungie-api/plugin.js
+++ bungie-api/plugin.js
@@ -1,28 +1,13 @@
-const axios = require("axios");
+
 require("dotenv").config({ path: "../.env" });
 const session = require("@fastify/session");
 const cookie = require("@fastify/cookie");
 
-const settings = require("./session-settings.js")[process.env.NODE_ENV];
+const settings = require("./session-settings.js")(process.env.NODE_ENV).cookie;
 const endpoints = require("./endpoints");
-const buildSession = require("./session.js").buildSession;
 const schemaLoader = require("./schemas");
-
-const axiosbase = {
-    baseURL: "https://www.bungie.net/Platform",
-    headers: {
-        "X-API-Key": process.env.BUNGIE_API_KEY,
-        "User-Agent": "VexGate API/"+process.env.VERSION+" AppId/"+process.env.BUNGIE_CLIENT_ID
-    },
-    /*transformResponse: function(data){
-        if(data.type == "HTML")
-            return Promise.reject("Bungie API Service is currently unavailable");
-        if(data.ErrorCode !== 1)
-            return Promise.reject(data.ErrorStatus);
-        
-       console.log("axios result: "+data);
-    }*/
-};
+const { buildSession } = require("./session.js");
+const { buildAPIRequest } = require("./api.js");
 
 module.exports = (fastify, options, next) => {
     for(schema in schemaLoader){
@@ -34,25 +19,13 @@ module.exports = (fastify, options, next) => {
     fastify.decorateRequest("BClient", null); // Add a utility function for making requests to the api
     fastify.decorateRequest("InjectURI", InjectURIParameters);
     fastify.addHook("onRequest", (request, reply) => {
-        
         // language var will be how we determine if session is new. if it doesn't exist, this is a new user.
         // stopgap until i find a way to properly detect new session instances
         if(!request.session.language)
             request.session = buildSession(request.session); // if no session already exists for user, create one
-        request.BClient = axios.create(axiosbase); // fill utility function with axios instance. (done here to avoid global mutations.)
-        request.BClient.interceptors.response.use( (response) => {
-            // html i believe is text/html, check a bungie response to confirm. then switch this if to that
-            return response.headers["content-type"] !== "application/json" ? Promise.reject("Bungie Service is currently unavailable") 
-                : response.data.ErrorCode != 1 ? Promise.reject(response.data.ErrorStatus)
-                : response;
-        }, (error) =>{
-            console.log("Request was rejected: ");
-            console.log(error);
-        });
+        request.BClient = buildAPIRequest();
     });
-
     fastify.register(endpoints);
-    
     next();
 };
 
