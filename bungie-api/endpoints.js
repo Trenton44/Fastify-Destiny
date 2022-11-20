311546b5c1119bdf73e4a8fc100205aa4f007a57
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-20 12:37:54 -0600

    Added test for root endpoint -test functions override validation functions -implant an active profile to verify root response

--- bungie-api/endpoints.js
+++ bungie-api/endpoints.js
@@ -13,7 +13,7 @@ let user = (fastify, options, next) => {
     fastify.addHook('preHandler', async (request, reply) => {
         return validateSession(request.session)
         .then( (success) =>{
-            request.BClient.headers["Authorization"] = "Bearer "+request.session.accessToken;
+            request.BClient.defaults.headers["Authorization"] = "Bearer "+request.session.accessToken;
             return validateProfiles(request);
         })
         .catch( (error) => reply.code(400).send(error));
@@ -22,6 +22,7 @@ let user = (fastify, options, next) => {
     fastify.get("/", async (request, reply) => {
         // if user doesn't receive a response, this api is down
         // if user receives a BungieUnavailable error, request failed at preHandler, because bungie is down
+        // if user recieves a UserUnauthorized error, request failed at preHandler because user has not logged in
         // if user gets here, both api's are functional, return the active Profile.
         return request.session.activeProfile;
     });
--- bungie-api/endpoints.test.js
+++ bungie-api/endpoints.test.js
@@ -1,13 +1,38 @@
-require("dotenv").config({ path: "../.env" });
-process.env.NODE_ENV="testing";
-const opts = require("../settings.js")(process.env.NODE_ENV);
+require("dotenv").config({ path: ".env" });
+process.env.NODE_ENV="testing"
+const session = require("./session.js");
 const build = require("../app.js");
-let app = build(opts);
+const opts = require("../settings.js")(process.env.NODE_ENV);
+const app = build(opts);
 
-test("accessing an invalid endpoint should return a 404 error", async () => {
+jest.mock("./session.js", () => {
+    const original = jest.requireActual("./session.js");
+    return {
+        ...original,
+        buildSession: jest.fn(()=> {
+            let temp = original.buildSession();
+            temp.__querycode = true;
+            return temp;
+        }),
+        validateSession: jest.fn(async (session) => true), //completely skip validation
+        validateProfiles: jest.fn(async (request) => { //skip validation, but add a fake profile in to ensure / returns the correct value
+            request.session._user.active = "12345";
+            request.session._user.profiles = {
+                "12345": {
+                    "potato": "beans"
+                }
+            }
+        })
+    };
+});
+
+test("endpoint '/' should return the active profile", async () => {
     let result = await app.inject({
         method: "GET",
-        url: "/completelyinvalidendpointthatshouldntexist"
+        url: "/"
     });
-    expect(result.statusCode).toEqual(404);
-});
+    expect(result.statusCode).toEqual(200);
+    expect(result.json()).toEqual({
+        "potato": "beans"
+    });
+})
