e59d82cfb4b0060af8f76c68681d99134601df90
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-27 02:19:48 -0600

    Added manual mock for session_settings updated session unit tests to use new global helpers

---
+++ bungie-api/__mocks__/session_settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ...jest.requireActual("../session_settings.js"),
+    idGenerator: jest.fn((request) => "test"+process.env.JEST_WORKER_ID),
+    cookie: {
+        path: "/",
+        maxAge: 3600000,
+        secure: false,
+    },
+    store: require("connect-mongo").create({
+        mongoUrl: process.env.MONGO_DB_URL,
+        dbName: process.env.MONGO_DB_NAME,
+        collectionName: process.env.MONGO_DB_COLLECTION,
+        stringify: false,
+    })
+};
--- bungie-api/session.test.js
+++ bungie-api/session.test.js
@@ -1,79 +1,43 @@
-let mockAxios = jest.createMockFromModule("axios");
-jest.mock("axios", () => mockAxios);
-mockAxios.create = jest.fn(() => mockAxios); // session.js has internal axios.create instance. overriding it with mockAxios will allow us to mock it
+process.env.MONGO_DB_URL = global.__MONGO_URI__;
+jest.mock("./session_settings.js");
+let mongo = null;
+const app = global.buildServer();
 
-const session = require("./session.js");
-const { RefreshTokenExpired, UserUnauthorized } = require("./errortypes.js");
+beforeAll(async () => mongo = await global.connectDatabase(global.__MONGO_URI__));
+beforeEach(async () => {});
 
-describe("These tests validate the buildSession() functionality", () => {
-    const { buildSession } = require("./session.js");
-    const buildSessionMock = () => {
-        let temp = buildSession({
-            cookie: { maxAge: 20000 }
+describe("Ensure that the session cookie is properly set and session data appears in the database.", () => {
+    test("Accessing an endpoint should return a session cookie", async () => {
+        let request = await app.inject({
+            method: "GET",
+            url: "/"
         });
-        temp._user = mockUserData();
-        temp.authData = mockAuthData();
-    };
-    test("Running buildSession() should return a new session object", 
-        () => expect(buildSession()).not.toBe(buildSession())
-    );
-    test("Session language should default to 'en'",
-        () => expect(buildSession()._user.language).toEqual("en")
-    );
+        // validate that session cookie has been set.
+        expect(request.cookies).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    name: process.env.COOKIE_NAME
+                })
+            ])
+        );
+    });
+    
+    test("Corresponding session should exist inside DB", async () => {
+        let ses = await mongo.collection.find().toArray();
+        expect(ses).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    _id: "test"+process.env.JEST_WORKER_ID
+                })
+            ])
+        );
+    });
+
+    afterAll(async () => await mongo.collection.deleteMany({}));
 });
 
-test("validateSession() returns RefreshTokenExpired error if bungie api rejects refresh token.", async () => {
-    let mocksession = session.buildSession({
-        cookie: { maxAge: 1000 }
-    });
-    mocksession.authData = {
-        access_token: 1239078735421,
-        token_type: "access",
-        expires_in: -1000,
-        refresh_token: 12903958032,
-        refresh_expires_in: -1000
-    };
-    expect(() => session.validateSession(mocksession)).rejects.toBeInstanceOf(RefreshTokenExpired);
-});
-
-test("validateSession() returns UserUnauthorized if user session is unauthorized and does not contain a query code.", () => {
-    let mocksession = session.buildSession();
-    mocksession.isLoggedIn = false;
-    expect(session.validateSession(mocksession)).rejects.toBeInstanceOf(UserUnauthorized);
-});
-
-// TODO: Figure out how to mock axios instance
-test("validateSession() should check for access token on requests and refresh them from bungie api if expired.", async ()=> {
-    mockAxios.request.mockImplementationOnce(() => {
-        return Promise.resolve({
-            status: 200,
-            statusText: "OK",
-            headers: {
-                "content-type": "application/json"
-            },
-            data: {
-                access_token: 1239078735421,
-                token_type: "access",
-                expires_in: 42069,
-                refresh_token: 12903958032,
-                refresh_expires_in: 22305
-            }
-        });
-    });
-    let mocksession = session.buildSession({
-        cookie: { maxAge: 1000 }
-    });
-    mocksession.isLoggedIn = true;
-    mocksession.authData = {
-        access_token: 234578652,
-        token_type: "access",
-        expires_in: -10000,
-        refresh_token: 21654326586758,
-        refresh_expires_in: 10000
-    };
-    let result = await session.validateSession(mocksession);
-    expect(result).toEqual(true);
-    expect(mocksession.accessToken).toEqual(1239078735421);
-    expect(mocksession._authdata.refresh_token).toEqual(12903958032);
-    expect(mockAxios.request).toHaveBeenCalledTimes(1);
+afterEach(async () => {});
+afterAll(async () => {
+    await app.close();
+    await mongo.client.close();
 });
