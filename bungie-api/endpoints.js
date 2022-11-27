a591cec591b7ee6c2ecae7ed5adb9e4a4224d703
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-27 01:01:00 -0600

    Split session.js up to seperate concerns -session_auth now handles auth validation for required endpoints -login/loginresponse have been moved to their endpoints

--- bungie-api/endpoints.js
+++ bungie-api/endpoints.js
@@ -1,24 +1,33 @@
-const { validateSession, validateProfiles, BungieLogin, BungieLoginResponse } = require("./session");
+const { CheckSessionAuthorized, BungieLogin, BungieLoginResponse } = require("./session");
 const MapResponse = require("./api.js");
 
 let general = (fastify, options, next) => {
     fastify.get("/*", async (request, reply) => reply.code(404).send({ error: "Endpoint not found." }));
-    fastify.get("/login", BungieLogin);
-    fastify.get("/bnetResponse", BungieLoginResponse);
+    fastify.get("/login", (request, reply) => {
+        let redirect = new URL("https://www.bungie.net/en/OAuth/Authorize");
+        let state = require("crypto").randomBytes(16).toString("base64");
+        request.session.user._state = state;
+        redirect.search = new URLSearchParams({
+            client_id: process.env.BUNGIE_CLIENT_ID,
+            response_type: "code",
+            state: state
+        });
+        return reply.code(303).redirect(encodeURI(redirect));
+    });
+    fastify.get("/bnetResponse", (request, reply) => {
+        let valid = request.session.user._state === decodeURIComponent(request.query.state);
+        if(!valid){
+            request.session.destroy();
+            return reply.code(400).send({ error: "Invalid state parameter, user must re-authenticate." });
+        }
+        request.session.user._querycode = request.query.code;
+        return reply.code(303).redirect(process.env.ORIGIN);
+    });
     next();
 };
 
-
 let user = (fastify, options, next) => {
-    fastify.addHook('preHandler', async (request, reply) => {
-        return validateSession(request.session.user)
-        .then( (success) =>{
-            request.BClient.defaults.headers["Authorization"] = "Bearer "+request.session.user.accessToken;
-            return validateProfiles(request);
-        })
-        .catch( (error) => reply.code(400).send(error));
-    });
-    
+    fastify.addHook("preHandler", CheckSessionAuthorized);
     fastify.get("/", async (request, reply) => {
         // if user doesn't receive a response, this api is down
         // if user receives a BungieUnavailable error, request failed at preHandler, because bungie is down
@@ -26,12 +35,8 @@ let user = (fastify, options, next) => {
         // if user gets here, both api's are functional, return the active Profile.
         return request.session.user.activeProfile;
     });
-    fastify.get("/UserProfiles", async (request, reply) => {
-        return request.session.user.userProfiles;
-    });
-    fastify.post("/UserProfiles", async (request, reply) => {
-        //code to validate request and update active user here.
-    });
+    fastify.get("/UserProfiles", async (request, reply) => request.session.user.userProfiles);
+    fastify.post("/UserProfiles", async (request, reply) => Error("Endpoint under construction"));
     fastify.get("/GetProfile", { schema: {"$ref": "/input/GetProfile#" } }, async (request, reply) => {
         const openapiuri = "/Destiny2/{membershipType}/Profile/{destinyMembershipId}/";
         let profile = request.session.user.activeProfile;
@@ -46,9 +51,8 @@ let user = (fastify, options, next) => {
     });
     next();
 };
-
 module.exports = (fastify, options, next) => {
     fastify.register(general);
     fastify.register(user);
     next();
-};
+}
---
+++ bungie-api/session_auth.js
@@ -0,0 +1,67 @@
+const axiosToken = require("axios").create({
+    baseURL: "https://www.bungie.net/Platform/App/OAuth/token/",
+    method: "POST",
+    data: new URLSearchParams({
+        client_secret: process.env.BUNGIE_CLIENT_SECRET,
+        client_id: process.env.BUNGIE_CLIENT_ID,
+    })
+});
+
+async function validateSession(user){
+    if(!user.isLoggedIn){
+        if(!user._querycode)
+            return Promise.reject(new UserUnauthorized("No auth data, and no query code, user is not authorized."));
+        await axiosToken.request({ 
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            data: {
+                "grant_type": "authorization_code",
+                "code": user._querycode
+            }
+        }).then( (result) => user.authData = result.data)
+        .catch( (error) => Error(error));
+    }
+    let expire = user.tokenExpirations;
+    if(Date.now() > expire.access){
+        if(Date.now() > expire.refresh)
+            return Promise.reject(new RefreshTokenExpired("Refresh token has expired, user will need to re-authenticate."));
+        await axiosToken.request({ 
+            headers: {"X-API-Key": process.env.BUNGIE_API_KEY },
+            data: {
+                "grant_type": "refresh_token",
+                "refresh_token": user._authdata.refresh_token
+            }
+        }).then( (result) => user.authData = result.data)
+        .catch( (error) => Error(error));
+    }
+    return true;
+}
+async function validateProfiles(request){
+    let user = request.session.user;
+    if(user.activeProfile)
+        return true;
+    if(user.availableProfileIds.length != 0){
+        user.activeProfile = user.availableProfileIds[0];
+        return true;
+    }
+    // if here, there are no user profiles, we need to retrieve them from bungie
+    const openapiurl = "/User/GetMembershipsById/{membershipId}/{membershipType}/";
+    let uri = request.InjectURI(openapiurl, {
+        membershipId: user._user.membershipId,
+        membershipType: -1
+    });
+    let response = await request.BClient(uri)
+    .then( (resp) => MapResponse(resp, openapiurl, "UserData", user._user.language))
+    user._user.profiles = response.destinyMemberships;
+    user.activeProfile = response.primaryMembershipId ? 
+        response.primaryMembershipId : Object.keys(user.availableProfileIds)[0];
+    return true;
+}
+
+module.exports = (request, reply) => {
+    return validateSession(request.session.user)
+    .then( (success) =>{
+        request.BClient.defaults.headers["Authorization"] = "Bearer "+request.session.user.accessToken;
+        return validateProfiles(request);
+    })
+    .catch( (error) => reply.code(400).send(error));
+}
