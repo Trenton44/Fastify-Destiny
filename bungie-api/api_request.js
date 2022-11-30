8d2b1eee22530daf9e8150b05f39995cb6ba0175
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-29 22:08:55 -0600

    Refactored api_request -access token is now appended to headers if passed Moved InjectURIParameters() to module -request function now returns unfiltered response by default -Processes data if a config object is passed

--- bungie-api/api_request.js
+++ bungie-api/api_request.js
@@ -1,21 +1,30 @@
 const axios = require("axios");
+const BungieResponse = require("./api_response.js");
 
-const axiosbase = {
-    baseURL: "https://www.bungie.net/Platform",
-    headers: {
-        "X-API-Key": process.env.BUNGIE_API_KEY,
-        "User-Agent": "VexGate API/"+process.env.VERSION+" AppId/"+process.env.BUNGIE_CLIENT_ID
-    }
-};
-
-const interceptHTML = (response) => {
+const HTMLInterceptor = (response) => {
     return response.headers["content-type"] !== "application/json" ? Promise.reject("Bungie Service is currently unavailable") 
         : response.data.ErrorCode != 1 ? Promise.reject(response.data.ErrorStatus)
         : response;
 };
 
-module.exports = () => {
-    let axiosBungie = axios.create(axiosbase);
-    axiosBungie.interceptors.response.use(interceptHTML);
-    return axiosBungie;
-};
+const axiosBungie = axios.create({
+    baseURL: "https://www.bungie.net/Platform",
+    headers: {
+        "X-API-Key": process.env.BUNGIE_API_KEY,
+        "User-Agent": "VexGate API/"+process.env.VERSION+" AppId/"+process.env.BUNGIE_CLIENT_ID
+    }
+});
+axiosBungie.interceptors.response.use(HTMLInterceptor);
+
+function InjectURIParameters(uri, parameters){
+    for(let parameter in parameters)
+        uri = uri.replace("{"+parameter+"}", parameters[parameter]);
+    return uri;
+}
+
+module.exports = async (schemauri, parameters, language, token=null, config=null) => {
+    let uri = InjectURIParameters(schemauri, parameters);
+    let options = !token ? {} : { headers:{ "Authorization": "Bearer "+ token } };
+    let data = await axiosBungie(uri, options).catch((error) => Promise.reject(error));
+    return !config ? data : BungieResponse(data, schemauri, config, language);
+}
