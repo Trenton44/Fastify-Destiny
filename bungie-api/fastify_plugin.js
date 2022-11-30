ac24381087debde34c9412a4fadf43f7bf165378
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-11-29 18:26:44 -0600

    Updated imports for session files & bungie API -server now generates a UserSession with session data on every request

--- bungie-api/fastify_plugin.js
+++ bungie-api/fastify_plugin.js
@@ -1,37 +1,24 @@
 const cookie = require("@fastify/cookie");
 const session = require("@fastify/session");
 
-const sessionStore = require("./session_store.js");
-const sessionOptions = require("./session.js")(sessionStore);
-const sessionTemplate = require("./session.js").template;
+const sessionStore = require("./session/store.js");
+const sessionOptions = require("./session/settings.js")(sessionStore);
+const SessionClass = require("./session/user.js");
 
 const registerEndpoints = require("./endpoints.js");
 const schemaLoader = require("./schemas");
-const axiosBungie = require("./api_request.js");
+const BungieAPI = require("./api_request.js");
 
 module.exports = (fastify, options, next) => {
     for(schema in schemaLoader){
         fastify.addSchema(schemaLoader[schema]);
     }
-
     fastify.register(cookie); // register cookie to be used with bungie api session
     fastify.register(session, sessionOptions); // register session storage for bungie api.
-    fastify.decorateRequest("BClient", null); // Add a utility function for making requests to the api
-    fastify.decorateRequest("InjectURI", InjectURIParameters);
+    fastify.decorateRequest("BClient", BungieAPI); // Add a utility function for making requests to the api
     fastify.addHook("onRequest", async (request, reply) => {
-        // user var will be how we determine if session is new. if it doesn't exist, this is a new user.
-        // stopgap until i find a way to properly detect new session instances
-        if(!request.session.user)
-            request.session.user = sessionTemplate(request.session); // if no session already exists for user, create one
-        request.BClient = axiosBungie();
+        request.session.data = new SessionClass(request.session.data);
     });
     registerEndpoints(fastify);
     next();
 };
-
-
-function InjectURIParameters(uri, params){
-    for(parameter in params)
-        uri = uri.replace("{"+parameter+"}", params[parameter]);
-    return uri;
-}
