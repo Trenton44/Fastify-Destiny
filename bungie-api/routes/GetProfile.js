ed7b98b0db33f3db7a4e502a2a6b0c7caab1ca73
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-01 21:10:13 -0600

    Added api_response params to GetProfile endpoint

--- bungie-api/routes/GetProfile.js
+++ bungie-api/routes/GetProfile.js
@@ -23,15 +23,35 @@ module.exports = {
     handler: GET
 }
 
+const specuri = "/Destiny2/{membershipType}/Profile/{destinyMembershipId}/";
+const defaultConfig = {
+    "splice": true,
+    "x-enum-reference": false,
+    "Destiny.Responses.DestinyProfileResponse": {
+        "x-mapped-definition": true,
+        "group": {
+            "characterdata": ["characters", "characterInventories", "characterProgressions", "characterRenderData", "characterActivities", "characterEquipment", "characterKiosks", "characterPlugSets", "characterPresentationNodes", "characterRecords", "characterCollectibles", "characterStringVariables", "characterCraftables", "characterCurrencyLookups"],
+            "profiledata": ["profileInventory", "profileCurrencies", "profile", "platformSilver", "profileKiosks", "profilePlugSets", "profileProgression", "profilePresentationNodes", "profileRecords", "profileCollectibles", "profileTransitoryData", "profileStringVariables"]
+        }
+    },
+    "Destiny.Entities.Inventory.DestinyInventoryComponent": { "link": "key" },
+    "Destiny.Entities.Characters.DestinyCharacterComponent": { "link": "key" },
+    "Destiny.Entities.Characters.DestinyCharacterRenderComponent": { "link": "key" },
+    "Destiny.Entities.Items.DestinyItemComponent": 
+        { "filter": ["itemHash", "bucketHash", "itemHashMapped", "bucketHashMapped"] },
+    "SingleComponentResponseOfDestinyInventoryComponent": 
+        { "x-mapped-definition": false },
+};
+
 async function GET(request, reply){
-    const openapiuri = "/Destiny2/{membershipType}/Profile/{destinyMembershipId}/";
     let profile = request.session.user.activeProfile;
-    let response = await request.BClient(
-        request.InjectURI(openapiuri,{
+    let response = await request.BClient(specuri, {
+        params: {
             membershipType: profile.membershipType,
-            destinyMembershipId: profile.destinyMembershipId
-        }), 
-        { params: { components: request.query.components }}
-    ).catch( (error) => console.log(error));
+            destinyMembershipId: profile.destinyMembershipId,
+            components: request.query.components
+        }
+    });
     console.log(response);
+    request.JSONMap(response, specuri, defaultConfig, request.session.data.language);
 }
