0703d52b23b2608a556c36a107a3ef58b69fc4d1
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-04 12:55:03 -0600

    removed api_response, refactoring code into /utils

--- bungie-api/api_response.js
+++
@@ -1,8 +0,0 @@
-const guide = require("./utils/json-traverse.js");
-
-module.exports = function(data, uri, config, language){
-    if(!config || !data || !uri)
-        throw Error("You need to pass the required arguments.");
-    let respschema = guide.findPath(oalink);
-    return config == {} ? data : new DataMap(config, userlang).map(response.data, respschema);
-}
