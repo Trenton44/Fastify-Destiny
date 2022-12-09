2826c259f8843cc4e0aa4d40df378a64e698b4e7
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-08 18:03:20 -0600

    Updated config generator -Maps all keys to one level. -Each key includes only relevant custom keywords -Added "splice" as inheritable keyword

--- bungie-api/utils/config-generator.js
+++ bungie-api/utils/config-generator.js
@@ -1,5 +1,5 @@
 const keywords = ["splice", "group", "filter", "link", "x-enum-reference", "x-mapped-definition"];
-const inheritables = ["x-enum-reference", "x-mapped-definition"];
+const inheritables = ["splice", "x-enum-reference", "x-mapped-definition"];
 
 const inheritValues = (data, inherited) => {
     Object.entries(inherited).forEach(([key, value]) => {
@@ -8,7 +8,24 @@ const inheritValues = (data, inherited) => {
     });
     return data;
 };
-export default function* genFlatConfig(uri, data){
+ /**
+  * Generator function that flattens JSON Config object to a single level, with values containing relevant keyword data for processing.
+  * yields one sublevel at a time, starting from leaf nodes and working upwards.
+  * @param {String} uri - string showing original location within JSON
+  * @param {Obj} data - JSON obj
+  */
+export default function* generatorConfig(uri, data){
+    for(let key in data){
+        // if it's a custom keyword, skip it, no need to do processing.
+        if(keywords.find(keyword => keyword == key))
+            continue;
+        data[key] = inheritValues(data[key], data);
+        yield* generatorConfig(uri+'/'+key, data[key]);
+    }
+    yield [uri, Object.fromEntries(Object.entries(data).filter(([key, value]) => keywords.find(keyword => keyword == key)))];
+}
+
+/*export default function* genFlatConfig(uri, data){
     // maybe have it import a list of "keywords" from the relevant schema, and use those instead of custom keywords
     for(let key in data){
         if(keywords.find(element => element == key)){
@@ -18,21 +35,4 @@ export default function* genFlatConfig(uri, data){
         data[key] = inheritValues(data[key], data); //level above (data) has values to inherit. check for them and update this data obj.
         yield* genFlatConfig(uri+'/'+key, data[key]);
     }
-};
-
-//returns obj keyed by every part of the uri that needs to be altered.
-export function* genConfigKeys(uri, data){
-    // maybe have it import a list of "keywords" from the relevant schema, and use those instead of custom keywords
-    for(let key in data){
-        if(keywords.find(element => element == key))
-            continue;
-        data[key] = inheritValues(data[key], data);
-        yield [uri+'/'+key, data[key]];
-        yield* genConfigKeys(uri+'/'+key, data[key]);
-    }
-    return null;
-};
-/*
-    parse all the way down, but at the last level, check if any non-keywords exist. 
-    if not, we've reached the bottom of the schema keywords, we yield that.
-*/
+};*/
