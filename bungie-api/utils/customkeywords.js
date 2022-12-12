fcbd3e263919597b77f1d5a8cbbf28bd2e62ea36
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-11 20:44:06 -0600

    custom keywords for ConfigMap

--- bungie-api/utils/customkeywords.js
+++ bungie-api/utils/customkeywords.js
@@ -1,10 +1,41 @@
-const customkeywords = {
-    "x-mapped-definition": XMappedDefintition
+const keywords = {
+    //"x-mapped-definition": XMappedDefintition,
+    //"x-enum-reference": XEnumReference,
+    "filter": Filter,
+    "group": Group,
+    //"link": Link
 };
 
-
-function XMappedDefintition(){
-
+function Filter(data, schema, datakey, searchkey, option){
+    let dkey = datakey.split("/").pop();
+    if(!option.find(filterkey => filterkey === dkey))
+        delete data[datakey];
+    return data;
 }
+function Group(data, schema, datakey, searchkey, option){
+    for(let group in option){
+        for(let key in option[group]){
+            //let datakey.find(elem => key === elem);
+        }
+    }
+    return data;
+}
+/*
+function test(data, schema, datakey, dataregex, option, additionalProperties){
+    //x-mapped:
+    if(!schema["x-mapped-definition"] && !option)
+        return data;
+    let newkey = datakey+"Mapped";
+    let manifest = getManifest(language);
+    let datalocation = this.locate(schema["x-mapped-defintion"]["$ref"], manifest);
+    data[newkey] = datalocation;
+    //return data;
 
-export default customkeywords;
+
+    //filter
+    let dkey = datakey.slice(datakey.findLastIndex("/"), datakey.length - 1);
+    if(!option.find(filkey => filkey === dkey))
+        delete data[datakey];
+    return data;
+}*/
+export default keywords;
