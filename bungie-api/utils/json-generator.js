d508cb45a69dd4359c324e1ea76536193e34ee32
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-13 23:24:01 -0600

    Cleanup /utils and remove unecessary files

--- bungie-api/utils/json-generator.js
+++
@@ -1,9 +0,0 @@
-export default function* genFlatJSON(uri, data){
-    for(let key in data){
-        let temp = [uri+"/"+key, data[key]];
-        if(data[key] instanceof Object)
-            yield* genFlatJSON(...temp);
-        else { yield temp; }
-    }
-    return null;
-}
