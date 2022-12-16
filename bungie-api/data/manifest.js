a93b00d87ac40c32549284a6ee36bb2eead2c446
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2022-12-15 18:15:25 -0600

    Added function to fetch/read files from manifest data

--- bungie-api/data/manifest.js
+++ bungie-api/data/manifest.js
@@ -1,15 +1,13 @@
-import { readFile } from "node:fs/promises";
+import { readFileSync } from "node:fs";
 import languages from "./languages.json" assert { type: "json" };
-/*
-For now, loading all manifests into memory takes ~4GB, which isn't feasible
-plan is to load these into seperate mongodb using mongoimport/export and make specific requests from there.
-For now, only load "en" manifest.
-let manifests = {};
 
-async function loadManifests(){
-    for(let key in languages){
-        manifests[key] = await readFile(new URL("./manifests/"+key+".json", import.meta.url), { encoding: "utf8" });
-    }
-}*/
-const manifest = await readFile(new URL("./manifests/en.json", import.meta.url), { encoding: "utf8" });
-export default manifest;
+function loadData(keys, language="en"){
+    keys.split("/");
+    keys.shift();
+    keys = keys.join(".");
+    let data = null;
+    try{ data = readFileSync(new URL("./manifests/"+language+"/"+keys+".json", import.meta.url), { encoding: "utf8" }); }
+    catch{ data = null; }
+    return data;
+}
+export default loadData;
