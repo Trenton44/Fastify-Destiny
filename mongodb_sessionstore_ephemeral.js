5d292a771e5da8e1534d854d7a95f9364cc4b3e6
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-03-20 20:20:08 -0500

    updates to mongodb local export and package-lock

--- mongodb_sessionstore_ephemeral.js
+++ mongodb_sessionstore_ephemeral.js
@@ -2,9 +2,10 @@
     Note: This server is ephemeral, and will destroy itself on server shutdown
     These options are stored seperately here, as they are used in both the jest testing framework & the dev environment
 */
-export default DB_INSTANCE_OPTIONS = {
+const DB_INSTANCE_OPTIONS = {
     instance: {
         ip: process.env.HOST,
         dbName: process.env.MONGO_DB_NAME,
     }
 };
+export default DB_INSTANCE_OPTIONS;
